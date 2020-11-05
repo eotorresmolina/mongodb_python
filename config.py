0de91c449c0c1a26b9bca0505956e50cda6387cb
eotorresmolina <64936281+eotorresmolina@users.noreply.github.com>
2020-11-04 22:14:34 -0300

    Update

---
+++ config.py
@@ -0,0 +1,30 @@
+'''
+Descripción:
+Programa creado para leer archivos de 
+configuración: ===> config.ini
+'''
+
+__author__ = "Emmanuel O. Torres Molina"
+__email__ = "emmaotm@gmail.com"
+__version__ = "1.1"
+
+
+from configparser import ConfigParser
+
+
+def config(section, filename='config.ini'):
+
+    # Read Config File:
+    parser = ConfigParser()
+    parser.read(filename)
+
+    # Read Section:
+    config_param = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            config_param[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+
+    return config_param
